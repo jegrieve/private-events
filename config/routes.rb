fdde7bcc20357dab2eb1cadf7f6e69ae22d1632a
James Grieve <pocojames@hotmail.com>
2020-09-28 20:55:50 -0700

    add basic events views, routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:new, :create, :show]
+  resources :events, only: [:new, :create, :show, :index]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_230341) do
+ActiveRecord::Schema.define(version: 2020_09_29_032759) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "creator_id"
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
