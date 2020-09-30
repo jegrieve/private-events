3ebebb02bc139f913dd1b368613ce129a1d5f8b6
James Grieve <pocojames@hotmail.com>
2020-09-29 21:53:16 -0700

    add meetings model, associations between event, user and meeting

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_032759) do
+ActiveRecord::Schema.define(version: 2020_09_30_045002) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "creator_id"
     t.string "description"
+  end
+
+  create_table "meetings", force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
