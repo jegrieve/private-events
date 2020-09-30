81014845893e69948cdf3f384d8e0ad3410d79f3
James Grieve <pocojames@hotmail.com>
2020-09-29 22:27:39 -0700

    add root to events index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:new, :create, :show]
   resources :events, only: [:new, :create, :show, :index]
+
+  root "events#index"
 end
