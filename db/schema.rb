689296cb43de7b4374e9ffa91498cec17c57f958
James Grieve <pocojames@hotmail.com>
2020-09-29 22:16:44 -0700

    add to events and users views

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_045002) do
+ActiveRecord::Schema.define(version: 2020_09_30_051043) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "creator_id"
     t.string "description"
+    t.date "date"
   end
 
   create_table "meetings", force: :cascade do |t|
